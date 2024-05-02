019c82fa4c4d2ece37c58a1072086bfff2809ee1
nikita <nikita.lavrentyev@mysky.com>
2024-05-02 19:47:16 +0400

    fix tests

--- __tests__/blogs/blogs.e2e.test.ts
+++ __tests__/blogs/blogs.e2e.test.ts
@@ -1,7 +1,8 @@
-import { req } from '../test-helpers';
+import { createAuthorizationHeader, req } from '../test-helpers';
 import { db } from '../../src/db/db';
 import { HTTP_STATUSES, PATH_URL } from '../../src/utils/consts';
 import * as data from './datasets';
+import { SETTINGS } from '../../src/utils/settings';
 
 describe(`Endpoint (GET) - ${PATH_URL.BLOGS}`, () => {
   beforeEach(async () => {
@@ -52,7 +53,9 @@ describe(`Endpoint (POST) - ${PATH_URL.BLOGS}`, () => {
   });
 
   it('Should add blog', async () => {
-    const res = await req.post(PATH_URL.BLOGS).send(data.dataSetNewBlog).expect(HTTP_STATUSES.CREATED_201);
+    const res = await req.post(PATH_URL.BLOGS)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog).expect(HTTP_STATUSES.CREATED_201);
 
     expect(res.body).toEqual(expect.objectContaining(data.dataSetNewBlog));
 
@@ -62,49 +65,65 @@ describe(`Endpoint (POST) - ${PATH_URL.BLOGS}`, () => {
   });
 
   it('Should get Error while field "name" is too long', async () => {
-    const res = await req.post(PATH_URL.BLOGS).send(data.dataSetNewBlog1).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.post(PATH_URL.BLOGS)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog1).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet1);
   });
 
   it('Should get Error while field "name" is not a string', async () => {
-    const res = await req.post(PATH_URL.BLOGS).send(data.dataSetNewBlog2).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.post(PATH_URL.BLOGS)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog2).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet2);
   });
 
   it('Should get Error while field "name" is empty', async () => {
-    const res = await req.post(PATH_URL.BLOGS).send(data.dataSetNewBlog3).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.post(PATH_URL.BLOGS)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog3).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet3);
   });
 
   it('Should get Error while field "description" is too long', async () => {
-    const res = await req.post(PATH_URL.BLOGS).send(data.dataSetNewBlog4).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.post(PATH_URL.BLOGS)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog4).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet4);
   });
 
   it('Should get Error while field "description" is not a string', async () => {
-    const res = await req.post(PATH_URL.BLOGS).send(data.dataSetNewBlog5).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.post(PATH_URL.BLOGS)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog5).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet5);
   });
 
   it('Should get Error while field "description" is empty', async () => {
-    const res = await req.post(PATH_URL.BLOGS).send(data.dataSetNewBlog6).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.post(PATH_URL.BLOGS)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog6).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet6);
   });
 
   it('Should get Error while field "websiteUrl" is not correct', async () => {
-    const res = await req.post(PATH_URL.BLOGS).send(data.dataSetNewBlog7).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.post(PATH_URL.BLOGS)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog7).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet7);
   });
 
   it('Should get Error while we add too many fields specified', async () => {
-    const res = await req.post(PATH_URL.BLOGS).send(data.dataSetNewBlog8).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.post(PATH_URL.BLOGS)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog8).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet8);
   });
@@ -118,7 +137,9 @@ describe(`Endpoint (PUT) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
   it('Should update blog', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    await req.put(`${PATH_URL.BLOGS}/${id}`).send(data.dataSetUpdateBlog).expect(HTTP_STATUSES.NO_CONTENT_204);
+    await req.put(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetUpdateBlog).expect(HTTP_STATUSES.NO_CONTENT_204);
 
     const blog = await db.getBlogById(id);
 
@@ -127,13 +148,17 @@ describe(`Endpoint (PUT) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
 
   it(`Should get error ${HTTP_STATUSES.NOT_FOUND_404}`, async () => {
 
-    await req.put(`${PATH_URL.BLOGS}/1`).send(data.dataSetUpdateBlog).expect(HTTP_STATUSES.NOT_FOUND_404);
+    await req.put(`${PATH_URL.BLOGS}/1`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetUpdateBlog).expect(HTTP_STATUSES.NOT_FOUND_404);
   });
 
   it('Should get Error while field "name" is too long', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    const res = await req.put(`${PATH_URL.BLOGS}/${id}`).send(data.dataSetNewBlog1).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.put(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog1).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet1);
   });
@@ -141,7 +166,9 @@ describe(`Endpoint (PUT) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
   it('Should get Error while field "name" is not a string', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    const res = await req.put(`${PATH_URL.BLOGS}/${id}`).send(data.dataSetNewBlog2).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.put(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog2).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet2);
   });
@@ -149,7 +176,9 @@ describe(`Endpoint (PUT) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
   it('Should get Error while field "name" is empty', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    const res = await req.put(`${PATH_URL.BLOGS}/${id}`).send(data.dataSetNewBlog3).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.put(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog3).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet3);
   });
@@ -157,7 +186,9 @@ describe(`Endpoint (PUT) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
   it('Should get Error while field "description" is too long', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    const res = await req.put(`${PATH_URL.BLOGS}/${id}`).send(data.dataSetNewBlog4).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.put(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog4).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet4);
   });
@@ -165,7 +196,9 @@ describe(`Endpoint (PUT) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
   it('Should get Error while field "description" is not a string', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    const res = await req.put(`${PATH_URL.BLOGS}/${id}`).send(data.dataSetNewBlog5).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.put(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog5).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet5);
   });
@@ -173,7 +206,9 @@ describe(`Endpoint (PUT) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
   it('Should get Error while field "description" is empty', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    const res = await req.put(`${PATH_URL.BLOGS}/${id}`).send(data.dataSetNewBlog6).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.put(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog6).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet6);
   });
@@ -181,7 +216,9 @@ describe(`Endpoint (PUT) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
   it('Should get Error while field "websiteUrl" is not correct', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    const res = await req.put(`${PATH_URL.BLOGS}/${id}`).send(data.dataSetNewBlog7).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.put(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog7).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet7);
   });
@@ -189,7 +226,9 @@ describe(`Endpoint (PUT) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
   it('Should get Error while we add too many fields specified', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    const res = await req.put(`${PATH_URL.BLOGS}/${id}`).send(data.dataSetNewBlog8).expect(HTTP_STATUSES.BAD_REQUEST_400);
+    const res = await req.put(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .send(data.dataSetNewBlog8).expect(HTTP_STATUSES.BAD_REQUEST_400);
 
     expect(res.body).toEqual(data.errorDataSet8);
   });
@@ -203,11 +242,15 @@ describe(`Endpoint (DELETE) - ${PATH_URL.BLOGS}${PATH_URL.ID}`, () => {
   it('Should delete blog', async () => {
     const id = await db.addBlog(data.dataSetNewBlog);
 
-    await req.delete(`${PATH_URL.BLOGS}/${id}`).expect(HTTP_STATUSES.NO_CONTENT_204);
+    await req.delete(`${PATH_URL.BLOGS}/${id}`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .expect(HTTP_STATUSES.NO_CONTENT_204);
   });
 
   it(`Should get error ${HTTP_STATUSES.NOT_FOUND_404}`, async () => {
 
-    await req.delete(`${PATH_URL.BLOGS}/1`).expect(HTTP_STATUSES.NOT_FOUND_404);
+    await req.delete(`${PATH_URL.BLOGS}/1`)
+      .set(createAuthorizationHeader(SETTINGS.ADMIN_AUTH_USERNAME, SETTINGS.ADMIN_AUTH_PASSWORD))
+      .expect(HTTP_STATUSES.NOT_FOUND_404);
   });
 });
--- __tests__/test-helpers.ts
+++ __tests__/test-helpers.ts
@@ -2,3 +2,11 @@ import { app } from '../src/app';
 import { agent } from 'supertest';
 
 export const req = agent(app);
+
+export const createAuthorizationHeader = (username?: string, password?: string) => {
+  const credentials = btoa(`${username}:${password}`);
+
+  return {
+    Authorization: `Basic ${credentials}`,
+  };
+};

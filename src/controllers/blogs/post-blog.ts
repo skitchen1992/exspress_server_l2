b8df275f413c621e6c595cf310dcbd39e0ef45cb
nikita <nikita.lavrentyev@mysky.com>
2024-05-09 19:25:45 +0400

    add tests to mongo DB

--- src/controllers/blogs/post-blog.ts
+++ src/controllers/blogs/post-blog.ts
@@ -6,12 +6,18 @@ import { mongoDB } from '../../db/database';
 import { BlogDbType } from '../../types/blog_types';
 import { blogsCollection } from '../../db';
 import { mapIdField } from '../../utils/helpers';
+import { PostDbType } from '../../types/post-types';
 
 type ResponseType = CreateBlogSchemaResponse | ResponseErrorSchema;
 
 export const postBlogController = async (req: RequestWithBody<CreateBlogSchema>, res: Response<ResponseType>) => {
   try {
-    const result = await mongoDB.add<BlogDbType>(blogsCollection, req.body);
+    const newBlog: BlogDbType = {
+      ...req.body,
+      createdAt: new Date().toISOString(),
+      isMembership: true,
+    };
+    const result = await mongoDB.add<BlogDbType>(blogsCollection, newBlog);
     const blog = await mongoDB.getById<BlogDbType>(blogsCollection, result.insertedId.toString());
 
     if (result.acknowledged && blog) {
